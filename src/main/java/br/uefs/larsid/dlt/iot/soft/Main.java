ec629391dafe34444e3441bcd8e1032736478f3e
AllanCapistrano <allan.capistrano3@gmail.com>
2022-10-10 23:15:00 -0300

    Feat: add CLI

--- src/main/java/br/uefs/larsid/dlt/iot/soft/Main.java
+++ src/main/java/br/uefs/larsid/dlt/iot/soft/Main.java
@@ -2,6 +2,10 @@ package br.uefs.larsid.dlt.iot.soft;
 
 import br.uefs.larsid.dlt.iot.soft.models.LedgerReader;
 import br.uefs.larsid.dlt.iot.soft.models.ZMQServer;
+import br.uefs.larsid.dlt.iot.soft.utils.CLI;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * @author Allan Capistrano
@@ -9,20 +13,74 @@ import br.uefs.larsid.dlt.iot.soft.models.ZMQServer;
  */
 public final class Main {
 
+  /*---------------------------------Properties-------------------------------*/
+  private static String bufferSize;
+  private static String socketProtocol;
+  private static String socketURL;
+  private static String socketPort;
+  private static String address;
+
+  /*--------------------------------------------------------------------------*/
+
   public static void main(String[] args) {
     System.out.println("Starting...");
 
     String[] topics = { "sn", "tx" };
 
+    readProperties(args);
+
     new LedgerReader(
       new ZMQServer(
-        128,
-        "tcp",
-        "172.19.0.2",
-        "5556",
-        "ZLGVEQ9JUZZWCZXLWVNTHBDX9G9KZTJP9VEERIIFHY9SIQKYBVAHIMLHXPQVE9IXFDDXNHQINXJDRPFDX",
+        Integer.parseInt(bufferSize),
+        socketProtocol,
+        socketURL,
+        socketPort,
+        address,
         topics
       )
     );
+  }
+
+  /**
+   * Realiza leitura das propriedades passadas por parâmetro ou resgata
+   * valores presentes no arquivo de propriedade.
+   *
+   * @param args String[] - Dados passados na execução do projeto.
+   */
+  public static void readProperties(String[] args) {
+    try (
+      InputStream input = Main.class.getResourceAsStream(
+          "tangle-monitor.properties"
+        )
+    ) {
+      if (input == null) {
+        printlnDebug("Sorry, unable to find controller.properties.");
+        return;
+      }
+
+      Properties props = new Properties();
+      props.load(input);
+
+      bufferSize =
+        CLI.getBufferSize(args).orElse(props.getProperty("bufferSize"));
+
+      socketProtocol =
+        CLI.getSocketProtocol(args).orElse(props.getProperty("socketProtocol"));
+
+      socketURL = CLI.getSocketURL(args).orElse(props.getProperty("socketURL"));
+
+      socketPort =
+        CLI.getSocketPort(args).orElse(props.getProperty("socketPort"));
+
+      address = CLI.getAddress(args).orElse(props.getProperty("address"));
+    } catch (IOException ex) {
+      printlnDebug(
+        "Sorry, unable to find sensors.json or not create pesistence file."
+      );
+    }
+  }
+
+  private static void printlnDebug(String str) {
+    System.out.println(str);
   }
 }
