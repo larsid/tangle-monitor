622725bf65fedc9bb19f62a4e153c22d6538ae7b
AllanCapistrano <allan.capistrano3@gmail.com>
2022-10-19 09:06:02 -0300

    Docs: add comments

--- src/main/java/br/uefs/larsid/dlt/iot/soft/models/ReadApi.java
+++ src/main/java/br/uefs/larsid/dlt/iot/soft/models/ReadApi.java
@@ -5,15 +5,31 @@ import org.iota.jota.dto.response.FindTransactionResponse;
 import org.iota.jota.dto.response.GetBundleResponse;
 import org.iota.jota.utils.TrytesConverter;
 
+/**
+ * @author Allan Capistrano
+ * @version 1.0.0
+ */
 public class ReadApi {
 
   private IotaAPI api;
 
+  /**
+   * Método construtor.
+   *
+   * @param protocol - Protocolo DLT
+   * @param url - URL DLT
+   * @param port - Porta DLT
+   */
   public ReadApi(String protocol, String url, int port) {
     this.api =
       new IotaAPI.Builder().protocol(protocol).host(url).port(port).build();
   }
 
+  /**
+   * Busca e exibe transações por uma TAG específica.
+   *
+   * @param tag - TAG que se deseja buscar.
+   */
   public void findTransactionsByTag(String tag) {
     String tagTrytes = TrytesConverter.asciiToTrytes(tag);
 
