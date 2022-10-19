662946398d98fa02182a16ee7925a10e1b75a959
AllanCapistrano <allan.capistrano3@gmail.com>
2022-10-19 09:02:32 -0300

    Feat: using ReadApi

--- src/main/java/br/uefs/larsid/dlt/iot/soft/Main.java
+++ src/main/java/br/uefs/larsid/dlt/iot/soft/Main.java
@@ -1,6 +1,7 @@
 package br.uefs.larsid.dlt.iot.soft;
 
 import br.uefs.larsid.dlt.iot.soft.models.LedgerReader;
+import br.uefs.larsid.dlt.iot.soft.models.ReadApi;
 import br.uefs.larsid.dlt.iot.soft.models.ZMQServer;
 import br.uefs.larsid.dlt.iot.soft.utils.CLI;
 import java.io.IOException;
@@ -19,6 +20,9 @@ public final class Main {
   private static String socketURL;
   private static String socketPort;
   private static String address;
+  private static String dltProtocol;
+  private static String dltURL;
+  private static String dltPort;
 
   /*--------------------------------------------------------------------------*/
 
@@ -39,6 +43,10 @@ public final class Main {
         topics
       )
     );
+
+    ReadApi api = new ReadApi(dltProtocol, dltURL, Integer.parseInt(dltPort));
+
+    api.findTransactionsByTag("lb_status");
   }
 
   /**
@@ -73,6 +81,14 @@ public final class Main {
         CLI.getSocketPort(args).orElse(props.getProperty("socketPort"));
 
       address = CLI.getAddress(args).orElse(props.getProperty("address"));
+
+      dltProtocol =
+        CLI.getDltProtocol(args).orElse(props.getProperty("dltProtocol"));
+
+      dltURL = CLI.getDltURL(args).orElse(props.getProperty("dltURL"));
+
+      dltPort =
+        CLI.getDltPort(args).orElse(props.getProperty("dltPort"));
     } catch (IOException ex) {
       printlnDebug(
         "Sorry, unable to find sensors.json or not create pesistence file."
